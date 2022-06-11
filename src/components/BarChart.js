0af3a663bb0052a5aaca446923241cea3938d1d4
unknown <bh.pro.pust@gmail.com>
2022-06-12 03:17:02 +0600

    there is a bug in select input

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,7 +9,7 @@ import {
   Legend
 } from 'chart.js'
 import {Bar} from 'react-chartjs-2'
-import {faker} from '@faker-js/faker'
+// import {faker} from '@faker-js/faker'
 
 
 ChartJS.register(
@@ -48,54 +48,90 @@ export const options = {
   },
 }
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgb(255, 99, 132)',
-      stack: 'Stack 0',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgb(75, 192, 192)',
-      stack: 'Stack 1',
-    },
-    {
-      label: 'Dataset 3',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgb(53, 162, 235)',
-      stack: 'Stack 2',
-    },
-  ],
-}
-
 
 const BarChart = () => {
-  const [val, setVal] = useState([30, 40])
   const [leftVal, setLeftVal] = useState(0)
   const [rightVal, setRightVal] = useState(11)
+
   const months = ['January','February','March', 'April', 'May', 'June', 'July','August','September','October','November','December']
-  
-  
+  const [labels, setLabels] = useState(months.slice(leftVal, rightVal+1))
+  const [dataset1, setDataset1] = useState([867, 238, 234, 838, 565, 967, 605, 300, 125, 521, 112, 181])
+  const [dataset2, setDataset2] = useState([665, 27, 801, 306, 854, 255, 231, 116, 719, 386, 486, 469])
+  const [dataset3, setDataset3] = useState([947, 789, 479, 908, 72, 688, 373, 676, 483, 172, 872, 832])
+  const [data, setData] = useState({
+    labels: labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [],
+        backgroundColor: 'rgb(255, 99, 132)',
+        stack: 'Stack 0',
+      },
+      {
+        label: 'Dataset 2',
+        data: [],
+        backgroundColor: 'rgb(75, 192, 192)',
+        stack: 'Stack 1',
+      },
+      {
+        label: 'Dataset 3',
+        data: [],
+        backgroundColor: 'rgb(53, 162, 235)',
+        stack: 'Stack 2',
+      },
+    ],
+  })
 
-  const handleChange = (e, data) => {
-    console.log(data)
-    setVal(data)
-  }
+  useEffect(() => {
+    setLabels(months.slice(leftVal, rightVal+1))
+    console.log(labels)
+    var newData = {
+      labels: months.slice(leftVal, rightVal+1),
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: dataset1.slice(leftVal, rightVal+1),
+          backgroundColor: 'rgb(255, 99, 132)',
+          stack: 'Stack 0',
+        },
+        {
+          label: 'Dataset 2',
+          data: dataset2.slice(leftVal, rightVal+1),
+          backgroundColor: 'rgb(75, 192, 192)',
+          stack: 'Stack 1',
+        },
+        {
+          label: 'Dataset 3',
+          data: dataset3.slice(leftVal, rightVal+1),
+          backgroundColor: 'rgb(53, 162, 235)',
+          stack: 'Stack 2',
+        },
+      ],
+    }
+    setData(newData)
+    console.log("setted data", data)
+    // console.log("newData", newData)
+  }, [dataset1, dataset2, dataset3, leftVal, rightVal])
 
-  const handleSelectChange = (e, data, selectType) => {
-    if(selectType==1){
-      setLeftVal(data)
+
+  const handleSelectChange = async(e, val, selectType) => {
+    let left = leftVal
+    let right = rightVal
+
+    if(selectType===1){
+      setLeftVal(val)
+      console.log("left=>", val, selectType, leftVal)
+      left = val
     }
-    else if(selectType==2){
-      setRightVal(data)
+    else if(selectType===2){
+      setRightVal(val)
+      console.log("right=>", val, selectType, rightVal)
+      right = val
     }
-    console.log(data)
+    // left = Math.min(left, right)
+    // right = Math.max(left, right)
+    // setLabels(months.slice(left, right+1))
+    console.log(left, right, labels, data)
   }
 
 	return (<div>
